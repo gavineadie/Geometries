90bdb39e352f6e3401aaacfd44c79e37606eefa9
Gavin Eadie <gavin@umich.edu>
2015-09-25 15:18:12 -0400

    Initial Commit

---
+++ Geometries/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Geometries
+//
+//  Created by Gavin Eadie on 9/25/15.
+//  Copyright © 2015 Gavin Eadie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Geometries/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Geometries
+//
+//  Created by Gavin Eadie on 9/25/15.
+//  Copyright © 2015 Gavin Eadie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
