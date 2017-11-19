b9fea346ba783f1ccfe97a4eec2d17253fa9494a
Gavin Eadie <gavin@umich.edu>
2017-11-19 00:43:21 -0500

    Re-add "FakeClock" ..

---
+++ Geometries/Sources/Utility/FakeClock.swift
@@ -0,0 +1,42 @@
+/*╔══════════════════════════════════════════════════════════════════════════════════════════════════╗
+  ║ FakeClock.swift                                                                       Satellites ║
+  ║ Created by Gavin Eadie on Aug26/17  ..  Copyright © 2017 Ramsay Consulting. All rights reserved. ║
+  ╚══════════════════════════════════════════════════════════════════════════════════════════════════╝*/
+
+import Foundation
+import SatKit
+
+class FakeClock {
+
+    typealias JulianDays = Double
+
+    static let sharedInstance = FakeClock()
+
+    public var dateOffset: Double = 0.0
+    public var dateFactor: Double = 0.0
+           var dateOrigin: Date = Date()
+
+    public func date() -> Date {
+
+        let now = Date()
+        return now + dateOffset + now.timeIntervalSince(dateOrigin) * dateFactor
+
+    }
+
+    public func reset() {
+
+        self.dateOffset = 0.0
+        dateFactor = 0.0
+        dateOrigin = Date()
+
+    }
+
+    public func julianDaysNow() -> JulianDays {
+        return JD.appleZero + self.date().timeIntervalSinceReferenceDate * TimeConstants.sec2day
+    }
+
+    public func ep1950DaysNow() -> Double {
+        return julianDaysNow() - JD.noradZero
+    }
+
+}
